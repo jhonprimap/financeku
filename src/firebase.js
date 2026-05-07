fa6a53555ae9a58a680c6f6ed043f98528728503
Jhon Primap <jhonprimap@gmail.com>
2026-05-07 20:37:34 +0700

    fix: move firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNakAtYl34mMd85nQLhGARk1XyP_-MZyQ",
-  authDomain: "financeku-cc5d2.firebaseapp.com",
-  projectId: "financeku-cc5d2",
-  storageBucket: "financeku-cc5d2.firebasestorage.app",
-  messagingSenderId: "800923787484",
-  appId: "1:800923787484:web:e50c390fc5129c66a35419"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app  = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
